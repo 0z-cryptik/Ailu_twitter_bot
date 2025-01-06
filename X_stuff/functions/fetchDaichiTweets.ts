e2d143472f5914d3972f665fef4863e048bcaf6d
Eniitan <enitanolawale0@gmail.com>
2025-01-06 16:15:04 +0100

    retained only the fetchTweets function

--- X_stuff/functions/fetchDaichiTweets.ts
+++ X_stuff/functions/fetchDaichiTweets.ts
@@ -3,38 +3,15 @@ import { config } from "dotenv";
 
 config({ path: "../../.env" });
 
-const BEARER_TOKEN = process.env.BEARER_TOKEN
+const BEARER_TOKEN = process.env.BEARER_TOKEN;
+const USER_ID = process.env.DAICHI_ACCOUNT_ID;
 
-
-const fetchUserId = async (): Promise<string | null> => {
-  const url = "https://api.twitter.com/2/users/by/username/CryptoDaichi";
-  const headers = { Authorization: `Bearer ${BEARER_TOKEN}` };
-
-  try {
-    const response = await fetch(url, { headers });
-
-    if (response.ok) {
-      const data = await response.json();
-      return data?.data?.id || null;
-    } else {
-      console.error("Error fetching user ID:", await response.json());
-      return null;
-    }
-  } catch (error) {
-    console.error("Fetch error:", error);
-    return null;
-  }
-};
-
-const fetchTweets = async (
-  userId: string,
-  maxResults: number = 100
-): Promise<string[]> => {
-  const url = `https://api.twitter.com/2/users/${userId}/tweets`;
+export const fetchTweets = async (): Promise<string[]> => {
+  const url = `https://api.twitter.com/2/users/${USER_ID}/tweets`;
   const headers = { Authorization: `Bearer ${BEARER_TOKEN}` };
 
   const params = new URLSearchParams({
-    max_results: maxResults.toString(),
+    max_results: "50",
     exclude: "replies,retweets"
   });
 
@@ -42,8 +19,10 @@ const fetchTweets = async (
     const response = await fetch(`${url}?${params.toString()}`, {
       headers
     });
+
     if (response.ok) {
       const data = await response.json();
+
       return (
         data?.data?.map((tweet: { text: string }) => tweet.text) || []
       );
@@ -57,22 +36,3 @@ const fetchTweets = async (
   }
 };
 
-(async () => {
-  // Fetch user ID
-  const userId = await fetchUserId();
-
-  if (userId) {
-    // Fetch and save tweets
-    const tweets = await fetchTweets(userId);
-
-    if (tweets.length > 0) {
-      console.log(tweets)
-    } else {
-      console.error("No tweets fetched or an error occurred.");
-    }
-  } else {
-    console.error("Failed to fetch user ID.");
-  }
-})();
-
-//1490204143104974849
